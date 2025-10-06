cce4af0755ad0a2ee779afb64f8571ad18fec44e
Mary <mary@LAPTOP-1P06IC7C.localdomain>
2025-10-06 21:09:20 +0800

    http_request_clone_2

--- http_request.rb
+++ http_request.rb
@@ -3,7 +3,7 @@
 def http_request(name, param = nil)
   if name == 'hccg'
     response = HTTP.get("https://opendata.hccg.gov.tw/API/v3/Rest/OpenData/45A6214813A52DED?take=#{param['take']}&skip=#{param['skip']}")
-  # insert other apis here
+  # insert other apis here 
   else
     raise 'Unknown API'
   end
