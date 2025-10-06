da54e30808f12994c3d34f8b9011c056d36549a1
linda123456789123456789 <linda109101503@g.ncu.edu.tw>
2025-10-06 20:06:31 +0800

    add: function 'convert_json_to_yaml'

--- hccg_spot.rb
+++ hccg_spot.rb
@@ -6,7 +6,8 @@ require 'json'
 require 'yaml'
 require 'fileutils'
 require_relative 'lib/http_request'
+require_relative 'json_to_yaml'
 
 page1 = http_request('hccg', { 'take' => 10, 'skip' => 0 })
-data = JSON.parse(page1.to_s)
-save_fixture('hccg-spots-data', data)
+yaml_path = File.join(__dir__, 'spec', 'fixtures', 'hccg-spots-data-results.yml')
+convert_json_to_yaml(page1.to_s, yaml_path)
